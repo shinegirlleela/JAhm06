cc2959e58e661671cff7228dd9c598335d505822
shinegirlleela <149800925+shinegirlleela@users.noreply.github.com>
2024-05-20 14:59:31 +0530

    Add files via upload

---
+++ first/emp.java
@@ -0,0 +1,22 @@
+package first;
+
+public class emp {
+	private String ename,email;
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ first/employee.java
@@ -0,0 +1,17 @@
+package first;
+
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		emp e1 = new emp();
+		
+		e1.setEname("leela");
+		e1.setEmail("leela123@gmail.com");
+		
+		System.out.println(e1.getEname() + " " +e1.getEmail() + " ");
+		
+	}
+
+}
